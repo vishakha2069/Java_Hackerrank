8aacbb7360190f5743ab2543134d9e4a20c7cad4
Vishakha <74141956+vishakha2069@users.noreply.github.com>
2021-03-15 20:09:27 +0530

    Create Helloworld1.java

---
+++ Helloworld1.java
@@ -0,0 +1,8 @@
+# Java_Hackerrank
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+    }
+}
