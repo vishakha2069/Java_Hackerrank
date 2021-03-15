c890f41574b06c96f03fbc40fea278eaae6776dd
Vishakha <74141956+vishakha2069@users.noreply.github.com>
2021-03-15 20:04:02 +0530

    Create DateTime.java

---
+++ DateTime.java
@@ -0,0 +1,90 @@
+# Java_Hackerrank
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+String weekDay=null;
+
+
+        try{
+
+
+        String date=day+"/"+month+"/"+year;
+
+
+        Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(date);
+
+
+        SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE");  
+
+
+        weekDay=simpleDateformat.format(date1).toUpperCase( );
+
+
+        }
+
+
+        catch(Exception ex)
+
+
+        {
+
+
+            System.out.println(ex);
+
+
+        }
+
+
+        return weekDay;
+
+
+    }
+
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
